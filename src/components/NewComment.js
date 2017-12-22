c1c207513f0fc430a11072e811bebce0942c630c
Alessa Messineo <apmessineo@gmail.com>
2017-12-22 16:11:23 -0500

    set up NewComment

--- src/components/NewComment.js
+++ src/components/NewComment.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
+import update from 'react-addons-update';
 
 
 export default class NewComment extends Component {
+  constructor(props){
+    super(props);
 
+    this.state = {
+      comment: {
+        comment_user: '',
+        comment_msg: ''
+      }
+    }
+  }
+
+handleChange(event){
+    let newState = update(this.state, {
+      comment: {
+        $merge: {
+          [event.target.name]: event.target.value
+        }
+      }
+    });
+
+    this.setState(newState);
+  }
+
+  handleSubmit(){
+    localStorage.setItem('newComment', JSON.stringify(this.state));
+    console.log('handleSubmit');
+  }
 
 
 
 render(){
     return(
-      <div></div>
+      <div className="new-comment">
+        <div className="comment-inputs">
+          <label>Reply Message: </label><br/>
+          <textarea name="comment-msg"
+          id="comment-input"
+          onChange={this.handleChange.bind(this)}
+          type='text'
+          />
+          <br /><br />
+          <label>Reply User: </label><br/>
+          <input name="comment-user"
+          onChange={this.handleChange.bind(this)}
+          type='text'
+          />
+        </div>
+        <button id="reply-btn" onClick={this.handleSubmit()}>Post Reply</button>
+      </div>
     )
   }
 }
